728fa4c00229985c65d5b9ee807e66728c35c46a
Le Quoc Loi <quocloihoak34ctn@gmail.com>
2026-07-08 10:52:46 +0700

    learn string

---
+++ learn_string/bai1.py
@@ -0,0 +1,12 @@
+ten_truong ="hoc vien cong nghe buu chinh vien thong"
+
+print(ten_truong[4:8])
+#Note : trong python co ca chi so am va duong , chi so duong giong c++ , con chi so am bat dau tu -1    
+print(ten_truong[0])
+print(ten_truong[-1])
+# ky thuat slicing ( giong  substr y het )
+a = "chao mung den voi binh nguyen vo tan"
+print("chuoi duoc cat ra la :" , a[:10][:9])
+#su dung toan tu % de dinh dang chuoi
+s = "binh nguyen vo tan"
+print("chao mung den voi %s !" % s)
