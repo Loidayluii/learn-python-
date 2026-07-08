5fbd7b1b898eb97ea7c7021faf39a655ac8fb69b
Le Quoc Loi <quocloihoak34ctn@gmail.com>
2026-07-08 17:15:22 +0700

    learn_set_dic

---
+++ learn_sets_dictionaries/bai1.py
@@ -0,0 +1,13 @@
+# set kha giong trong c++
+
+list1 = [1,2,3,4,5,6,7,7,8]
+
+my_set = set(list1)
+print(my_set)
+# them mot phan tu vao set thi su dung ham add()
+# xoa mot phan tu khoi set thi dung ham remove()
+# mot so ham tuong tu nhu trong c++ : union() , intersection()  , difference() : a \ b  , synmetric_difference() : thuoc 1 trong 2 set nhung khong nam trong ca 2 set 
+
+# set comprehensions
+binh_phuong = { x ** 2 for x in range(1,6)}
+print(binh_phuong)
