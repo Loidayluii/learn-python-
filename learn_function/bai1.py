472d0da4de48e373147d112e1abb098e1961427b
Le Quoc Loi <quocloihoak34ctn@gmail.com>
2026-07-09 01:17:38 +0700

    Update bai1.py

--- learn_function/bai1.py
+++ learn_function/bai1.py
@@ -1,4 +1,4 @@
-def happy_birthday(x,y):
+'''def happy_birthday(x,y):
     print(f"Happy birthday to {x}")
     print(f"your age are {y}")
     print ("all best wishes to you !")
@@ -7,10 +7,10 @@ def happy_birthday(x,y):
 happy_birthday("Bro",20)
 happy_birthday("Peter",18)
 happy_birthday("Celine",36)
-
+'''
 def sv(x,y,z):
     print(f"ten cua toi la {x}")
     print(f"toi nam nay {y} tuoi")
     print(f"gpa cua toi la {z:.2f}")
 
-    sv("Loi",19,3.9889)
+sv("Loi",19,3.9889)
