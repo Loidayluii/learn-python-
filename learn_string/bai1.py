061df68da448f5e6042f6069a057e3fa147e8fe1
Le Quoc Loi <quocloihoak34ctn@gmail.com>
2026-07-08 17:14:51 +0700

    string

--- learn_string/bai1.py
+++ learn_string/bai1.py
@@ -10,3 +10,22 @@ print("chuoi duoc cat ra la :" , a[:10][:9])
 #su dung toan tu % de dinh dang chuoi
 s = "binh nguyen vo tan"
 print("chao mung den voi %s !" % s)
+#noi chuoi bang f string 
+gia_but_bi = 2500  
+so_luong_but = 3
+gia_quyen_vo = 8000 
+tong_tien = f'Tong tien mua {so_luong_but} but bi va 1 quyen vo : {gia_but_bi * so_luong_but + gia_quyen_vo} vnd'
+print(tong_tien)
+# mot so ham dinh dang nen dung 
+   #capitalize() : viet_hoa_chu_cai_dau
+# giong nhu trong c++
+'''
+string s 
+for(int i= 0 ; i < s.size() ; i++){
+s[0] = toupper(s[0]);
+}'''
+# swapcase() : dao nguoc kieu chu viet hoa thanh thuong va nguoc lai 
+# title() : viet hoa chu dau va cac chu con lai viet thuong 
+
+#split() : tach chuoi theo theo 1 ky tu phan tach va tra ve danh sach cac chuoi con 
+# isalnum() , isdigit() , isalpha() .... 
